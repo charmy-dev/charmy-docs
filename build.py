3e6eb64e4d94d15dbf969ba68feaa0142e6f75bf
真_人工智障 <67090055+TotoWang-hhh@users.noreply.github.com>
2026-07-10 01:41:43 +0800

    Replace system calls with subprocess for commands
    Refactor build script to use subprocess for command execution.

--- build.py
+++ build.py
@@ -1,7 +1,11 @@
-from os import system
+import shlex
+import subprocess
 
-system("rmdir /s /q build")
-system("sphinx-build -b gettext ./source build/gettext")
-system("sphinx-intl update -p build/gettext -l en")
-system("sphinx-build -b html -D language=zh_CN ./source build/html/zh_CN")
-system("sphinx-build -b html -D language=en ./source build/html/en")
+def run(command: str) -> None:
+    print(f"> {command}")
+    subprocess.run(shlex.split(command), check=True)
+
+run("sphinx-build -b gettext ./source build/gettext")
+run("sphinx-intl update -p build/gettext -l en")
+run("sphinx-build -b html -D language=zh_CN ./source build/html/zh_CN")
+run("sphinx-build -b html -D language=en ./source build/html/en")
